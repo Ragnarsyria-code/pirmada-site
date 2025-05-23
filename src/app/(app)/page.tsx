e5872585c509bfd70045130b7d647d400132f86c
Pirmada <info@pirmada.com>
2025-05-23 12:36:20 +0400

    add comments to test

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,7 +3,7 @@ import ContactSection from "@/components/ui/Contact/Form"
 import Features from "@/components/ui/Features/FeaturesDesktop"
 import Hero from "@/components/ui/Hero";
 
-
+//add comments to test
 
 export default function ExamplePage() {
   return (
